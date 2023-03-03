8705e6c7a8548ef97ff8e1ce462a0a6475892087
Florine W. Dekker <florine@fwdekker.com>
2023-03-03 20:32:44 +0100

    mommy fixes some minor inconsistencies~ 🤎

--- update.sh
+++ update.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 set -e
+cd -P -- "$(dirname -- "$0")"
 
 BASE="https://github.com/FWDekker/mommy/archive"
 VERSION="${1:?Version not specified}"
 URL="$BASE/$VERSION.tar.gz"
 
-hash=$(curl -sSfL "$URL" | sha256sum | cut -f1 -d" ")
+hash="$(curl -sSfL "$URL" | sha256sum | cut -f1 -d" ")"
 
 sed -i -E "s|url .*|url \"$URL\"|; s|sha256 .*|sha256 \"$hash\"|;" mommy.rb
